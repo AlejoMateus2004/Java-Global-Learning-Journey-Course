82a06537d18be635c82fe6cc5fb3d63a90827b4c
alejandro_mateus <mmateus@copaair.com>
2025-05-06 12:45:06 -0500

    21.	Create Exception class which will be used into orElseThrow for Optional from getSubscriptionByBankCardNumber().

---
+++ Java-8-and-9-10-11PracticalTask/jmp-service-api/src/main/java/org/service/api/SubscriptionNotFoundException.java
@@ -0,0 +1,7 @@
+package org.service.api;
+
+public class SubscriptionNotFoundException extends RuntimeException {
+    public SubscriptionNotFoundException(String message) {
+        super(message);
+    }
+}
