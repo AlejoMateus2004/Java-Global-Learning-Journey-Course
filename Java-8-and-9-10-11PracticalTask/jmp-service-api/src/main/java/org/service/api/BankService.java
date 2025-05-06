b7eb12f61cdf42405ee5766871ea80c92fc047f0
alejandro_mateus <mmateus@copaair.com>
2025-05-06 12:42:17 -0500

    18.	Add a new default method for Service interface, which uses getAllUsers. Use LocalDate.now(), ChronoUnit and mapToLong.
    - double getAverageUsersAge();

--- Java-8-and-9-10-11PracticalTask/jmp-service-api/src/main/java/org/service/api/BankService.java
+++ Java-8-and-9-10-11PracticalTask/jmp-service-api/src/main/java/org/service/api/BankService.java
@@ -5,6 +5,8 @@ import org.dto.api.domain.BankCard;
 import org.dto.api.domain.Subscription;
 import org.dto.api.domain.User;
 
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 import java.util.Optional;
 
@@ -12,5 +14,12 @@ public interface BankService {
     void subscribe(BankCard bankCard);
     Optional<Subscription> getSubscriptionByBankCardNumber(String bankcardNumber);
     List<User> getAllUsers();
+    default double getAverageUsersAge() {
+        return getAllUsers().stream()
+                .mapToLong(user -> ChronoUnit.YEARS.between(user.getBirthday(), LocalDate.now()))
+                .average()
+                .orElse(0);
+    }
+
 
 }
