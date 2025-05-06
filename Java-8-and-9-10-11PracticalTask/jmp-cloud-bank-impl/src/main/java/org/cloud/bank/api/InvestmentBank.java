7d1f1a690c891f013d9dd707d265904e1f957a93
alejandro_mateus <mmateus@copaair.com>
2025-05-06 12:36:23 -0500

    7.	Complete task Implement Bank into jmp-cloud-bank-impl. Method should create a new class depending on the type:
    - RetailBank
    - InvestmentBank
    - CentralBank

---
+++ Java-8-and-9-10-11PracticalTask/jmp-cloud-bank-impl/src/main/java/org/cloud/bank/api/InvestmentBank.java
@@ -0,0 +1,20 @@
+package org.cloud.bank.api;
+
+
+import org.dto.api.domain.BankCard;
+import org.dto.api.domain.User;
+
+public class InvestmentBank {
+
+    public static class CreditBankCard extends BankCard {
+        public CreditBankCard(String number, User user) {
+            super(number, user);
+        }
+    }
+
+    public static class DebitBankCard extends BankCard {
+        public DebitBankCard(String number, User user) {
+            super(number, user);
+        }
+    }
+}
