4524e34ea636fc9a9e090cc4666303d547834622
alejandro_mateus <mmateus@copaair.com>
2025-05-06 12:29:10 -0500

    11.	Implement Service into jmp-cloud-service-impl. Use Stream API, You can use Memory/DB for data storing

---
+++ Java-8-and-9-10-11PracticalTask/jmp-cloud-service-impl/src/main/java/org/cloud/service/api/BankServiceImpl.java
@@ -0,0 +1,49 @@
+package org.cloud.service.api;
+
+
+import org.dto.api.domain.BankCard;
+import org.dto.api.domain.Subscription;
+import org.dto.api.domain.User;
+import org.service.api.BankService;
+import org.service.api.SubscriptionNotFoundException;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class BankServiceImpl implements BankService {
+
+    private List<Subscription> subscriptions = new ArrayList<>();
+    private List<User> users = new ArrayList<>();
+
+    @Override
+    public void subscribe(BankCard bankCard) {
+        Subscription subscription = new Subscription();
+        subscription.setBankcardNumber(bankCard.getNumber());
+        subscription.setStartDate(LocalDate.now());
+
+        subscriptions.add(subscription);
+
+        boolean userExists = users.stream()
+                .anyMatch(user -> user.equals(bankCard.getUser()));
+
+        if (!userExists) {
+            users.add(bankCard.getUser());
+        }
+    }
+
+    @Override
+    public Optional<Subscription> getSubscriptionByBankCardNumber(String bankcardNumber) {
+        return Optional.ofNullable(subscriptions.stream()
+                .filter(subscription -> subscription.getBankcardNumber().equals(bankcardNumber))
+                .findFirst()
+                .orElseThrow(() -> new SubscriptionNotFoundException("Subscription not found for bankcard number: " + bankcardNumber)));
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return users;
+    }
+
+}
