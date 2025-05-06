bf9ccd17aaf98b049f9869e9dcdd23d57367a99e
alejandro_mateus <mmateus@copaair.com>
2025-05-06 12:00:56 -0500

    1.	Create maven project with 5 modules

---
+++ Java-8-and-9-10-11PracticalTask/jmp-application/src/main/java/org/application/api/App.java
@@ -0,0 +1,51 @@
+package org.application.api;
+
+import org.bank.api.Bank;
+import org.dto.api.domain.BankCard;
+import org.dto.api.domain.BankCardType;
+import org.dto.api.domain.User;
+import org.service.api.BankService;
+
+import java.time.LocalDate;
+import java.util.ServiceLoader;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        ServiceLoader<Bank> bankLoader = ServiceLoader.load(Bank.class);
+        Bank bank = bankLoader.findFirst().orElseThrow();
+
+        ServiceLoader<BankService> serviceLoader = ServiceLoader.load(BankService.class);
+        BankService service = serviceLoader.findFirst().orElseThrow();
+
+        User user = new User();
+        user.setName("John");
+        user.setSurname("Doe");
+        user.setBirthday(LocalDate.of(1980, 1, 1));
+
+        BankCard bankCard = bank.createBankCard(user, BankCardType.CREDIT);
+        service.subscribe(bankCard);
+
+        System.out.println("Users subscribed: ");
+        service.getAllUsers().forEach(System.out::println);
+
+        System.out.println("Average user age: " + service.getAverageUsersAge());
+
+        System.out.println("Is user payable: " + BankService.isPayableUser(user));
+
+        System.out.println("Subscriptions started today:");
+        service.getAllSubscriptionsByCondition(s -> s.getStartDate().equals(LocalDate.now())).forEach(System.out::println);
+
+        System.out.println("Subscriptions by card number:");
+        service.getSubscriptionByBankCardNumber(bankCard.getNumber()).ifPresentOrElse(
+                subscription -> System.out.println("Subscription found: " + subscription),
+                () -> System.out.println("Subscription not found")
+        );
+
+    }
+}
