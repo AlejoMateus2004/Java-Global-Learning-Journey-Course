bb571810b071b5ab8f98ecca10353d0390cd4d5e
alejandro_mateus <mmateus@copaair.com>
2025-05-06 14:31:47 -0500

    24.	Add to module-info jar for each implementation module :

--- Java-8-and-9-10-11PracticalTask/jmp-cloud-bank-impl/src/main/java/module-info.java
+++ Java-8-and-9-10-11PracticalTask/jmp-cloud-bank-impl/src/main/java/module-info.java
@@ -1,6 +1,10 @@
 module jmp.cloud.bank.impl {
     requires transitive jmp.bank.api;
     requires jmp.dto;
-    provides org.bank.api.Bank with org.cloud.bank.api.BankImpl;
+    provides org.bank.api.Bank with
+            org.cloud.bank.api.InvestmentBank,
+            org.cloud.bank.api.CentralBank,
+            org.cloud.bank.api.RetailBank;
+
     exports org.cloud.bank.api;
 }
