766abbe1c9736f54fe77a4e7688fe04c3d863b0f
alejandro_mateus <mmateus@copaair.com>
2025-05-08 09:13:40 -0500

    Resolve comment, Refactor bank card creation logic by centralizing card factory map in BankUtil

--- Java-8-and-9-10-11PracticalTask/jmp-cloud-bank-impl/src/main/java/org/cloud/bank/api/BankUtil.java
+++ Java-8-and-9-10-11PracticalTask/jmp-cloud-bank-impl/src/main/java/org/cloud/bank/api/BankUtil.java
@@ -1,8 +1,25 @@
 package org.cloud.bank.api;
 
+import org.bank.api.BankCardFactory;
+import org.dto.api.domain.BankCardType;
+import org.dto.api.domain.CreditBankCard;
+import org.dto.api.domain.DebitBankCard;
+
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 public class BankUtil {
+    private static final Map<BankCardType, BankCardFactory> cardFactoryMap = new HashMap<>();
+
+    static {
+        cardFactoryMap.put(BankCardType.CREDIT, CreditBankCard::new);
+        cardFactoryMap.put(BankCardType.DEBIT, DebitBankCard::new);
+    }
+
+    public static Map<BankCardType, BankCardFactory> getBanCardFactoryMap() {
+        return cardFactoryMap;
+    }
     static String generateCardNumber() {
         return UUID.randomUUID().toString().replaceAll("-", "").substring(0, 16);
     }
--- Java-8-and-9-10-11PracticalTask/jmp-cloud-bank-impl/src/main/java/org/cloud/bank/api/CentralBank.java
+++ Java-8-and-9-10-11PracticalTask/jmp-cloud-bank-impl/src/main/java/org/cloud/bank/api/CentralBank.java
@@ -5,23 +5,14 @@ import org.bank.api.Bank;
 import org.bank.api.BankCardFactory;
 import org.dto.api.domain.*;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import static org.cloud.bank.api.BankUtil.generateCardNumber;
 
 public class CentralBank implements Bank {
-    private static final Map<BankCardType, BankCardFactory> cardFactoryMap = new HashMap<>();
-
-    static {
-        cardFactoryMap.put(BankCardType.CREDIT, CreditBankCard::new);
-        cardFactoryMap.put(BankCardType.DEBIT, DebitBankCard::new);
-    }
 
     @Override
     public BankCard createBankCard(User user, BankCardType cardType) {
         System.out.println("Service invoked in CentralBank");
-        BankCardFactory bankCardFactory = cardFactoryMap.get(cardType);
+        BankCardFactory bankCardFactory = BankUtil.getBanCardFactoryMap().get(cardType);
         String cardNumber = generateCardNumber();
 
         if (bankCardFactory != null) {
