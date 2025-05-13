7da916bb17bbf33821270612fb3d79ec619fc18f
alejandro_mateus <mmateus@copaair.com>
2025-05-13 10:35:36 -0500

    Translate comments in App.java to English for better clarity

--- Concurrency-and-Multithreading-Architecture-Module-Practical-Task/Task3/src/main/java/org/task3/App.java
+++ Concurrency-and-Multithreading-Architecture-Module-Practical-Task/Task3/src/main/java/org/task3/App.java
@@ -9,13 +9,12 @@ public class App
     public static void main(String[] args) {
         MessageBus messageBus = new MessageBus();
 
-        // Crear y arrancar productores
+        // Create and Start producers
         for (int i = 1; i <= 3; i++) {
             Producer producer = new Producer(messageBus, "Producer-" + i);
             new Thread(producer).start();
         }
-
-        // Crear y arrancar consumidores
+        // Create and Start consumers
         for (int i = 1; i <= 3; i++) {
             Consumer consumer = new Consumer(messageBus, "Consumer-" + i);
             new Thread(consumer).start();
